d1e48a4e6e90aa4b1dfa171546745b164246de99
yoga-suhas-km <yogasuhas.ys@gmail.com>
2020-01-29 14:48:53 -0500

    added check to include JPG files as well

--- multi-modal-face-identification/face_extraction.py
+++ multi-modal-face-identification/face_extraction.py
@@ -55,7 +55,7 @@ def extract_face(path_to_read_imgs, path_to_save_extracted_face, img_size_vertic
 def extract_and_save_face(path_to_read_imgs, path_to_save_extracted_face, image_size_vertical, image_size_horizontal):
     path_t, dirs_t, file_t = next(os.walk(path_to_read_imgs))
     for file in file_t: 
-        if file.endswith('.jpg'):
+        if file.endswith('.jpg') or file.endswith('.JPG'):
             abs_path = os.path.abspath(os.path.join(path_to_read_imgs, file))
             platform = sys.platform
             if "W" in platform:
